31f7453d8167adb253e8eb9ed8b8a68f9a98d1d8
mixailvelinov <mihail.vasilev.velinov@gmail.com>
2025-01-19 04:02:23 +0200

    add: styles for deposit forms and index

--- deposits/forms.py
+++ deposits/forms.py
@@ -8,3 +8,10 @@ class DepositCreateForm(forms.ModelForm):
         model = Deposit
         exclude = ['date', 'user']
 
+        widgets = {
+            'amount': forms.TextInput(attrs={'placeholder': 'What amount are you depositing?'}),
+            'description': forms.TextInput(attrs={'placeholder': 'I got the funds from...'}),
+        }
+
+
+
